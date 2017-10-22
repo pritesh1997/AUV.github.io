13a62b6019e93b38ea86757ce1f531fc15517a6d
pritesh1997 <32921122+pritesh1997@users.noreply.github.com>
2017-10-22 15:01:23 +0530

    AUV

---
+++ Java.js
@@ -0,0 +1,24 @@
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+var myIndex=1;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+       myIndex++;
+       if (myIndex > x.length) {myIndex = 1}
+       x[myIndex-1].style.display = "block";
+       setTimeout(carousel, 3000);
+}
